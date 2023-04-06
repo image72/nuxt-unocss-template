75be73e9a4196f289bd9769caa8feb2297b59bc7
image72 <image72@msn.com>
2023-04-06 08:30:55 +0000

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,34 @@
+<script lang="ts">
+export default {
+  name: "DefaultLayout",
+};
+</script>
+<script setup lang="ts">
+// Navigation for default theme
+const { loadNavigationElements } = useNavigation();
+const { data } = await useAsyncData("mainNavigation", () => {
+  return loadNavigationElements({ depth: 2 });
+});
+provide("swNavigation-main-navigation", data);
+
+const { loadNavigationElements: loadFooterNavigationElements } = useNavigation({
+  type: "footer-navigation",
+});
+const { data: footerData } = await useAsyncData("mainFooterNavigation", () => {
+  return loadFooterNavigationElements({ depth: 2 });
+});
+provide("swNavigation-footer-navigation", footerData);
+</script>
+<template>
+  <div>
+    <SwModal />
+    <SwHeader />
+    <SwNotifications />
+    <div class="mx-auto">
+      <SwBreadcrumbs />
+      <slot />
+    </div>
+    <SwSideCart />
+    <SwFooter />
+  </div>
+</template>
